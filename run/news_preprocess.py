4eae63dd8d40b91d4ffabed8c840d18f61e78f9a
Boris <blank54@snu.ac.kr>
2022-02-12 17:57:38 +0900

    re-init

--- run/news_preprocess.py
+++ run/news_preprocess.py
@@ -7,5 +7,18 @@ import sys
 rootpath = os.path.sep.join(os.path.dirname(os.path.abspath(__file__)).split(os.path.sep)[:-1])
 sys.path.append(rootpath)
 
-from newsutil import NewsPath
-newspath = NewsPath()
+from newsutil import NewsIO
+newsio = NewsIO()
+
+
+for idx, article in newsio.read_articles(iter='each'):
+    if idx == 5:
+        break
+    else:
+        print(article.url)
+        print(article.id)
+        print(article.query)
+        print(article.title)
+        print(article.date)
+        print(article.category)
+        print(article.content)
